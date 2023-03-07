82a211d6f1b4911d866b7fe902fa1ba68c234c45
Montywilliams <3650@holbertonschool.com>
2023-03-07 05:29:09 -0600

    correct import: for the nav bar we use a nav tag instead of a div for best practices: within the nav tag we use a cool lil syntax for the padding that inherits a class from the style sheet and also uses tailiwinds class structure

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,14 @@
 import React, {useStat, useEffect} from 'react'
 import { LInk } from 'react-router-dom'
-import { styles } from '../styles'
+import { styles } from '../../style'
 import { navLinks } from '../constants'
 import { logo, menu, close } from '../assets'
 
 const Navbar = () => {
   return (
-    <div>Navbar Bro Bro</div>
+    <nav className={`${styles.paddingX} w-full flrx items-center py-5 fixed top-0 z-20 bg-primary`}>
+
+    </nav>
   )
 }
 
