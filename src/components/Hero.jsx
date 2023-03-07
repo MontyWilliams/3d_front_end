c1e08364d6077dab379c2ba54b11bcb9328bb778
Montywilliams <3650@holbertonschool.com>
2023-03-07 12:38:03 -0600

    import requirements

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { motion } from 'framer-motion';
+import { styles } from '../../style'
+import { computersCanvas } from './canvas';
 
 const Hero = () => {
   return (
