75cb1d70fc90aff8effc9a1e87f1741801fc75fd
Montywilliams <3650@holbertonschool.com>
2023-03-07 14:02:24 -0600

    correct import to reflect dir changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react'
 import { Link } from 'react-router-dom'
-import { styles } from '../../style'
+import { styles } from '../style'
 import { navLinks } from '../constants'
 import { dm_color, menu, close } from '../assets'
 
