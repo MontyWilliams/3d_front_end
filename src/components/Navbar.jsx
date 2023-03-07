1cb57ff3d42a86f695d24d01451ebaa0fe32123c
Montywilliams <3650@holbertonschool.com>
2023-03-07 07:52:09 -0600

    create state for toggle menu: create toggle menu for small displays and add onClick to set toggle

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,6 +6,8 @@ import { dm_color, menu, close } from '../assets'
 
 const Navbar = () => {
   const [ active, setActive ] = useState('');
+  const [toggle, setToggle ] = useState('');
+
   return (
     <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
@@ -35,6 +37,15 @@ const Navbar = () => {
             </li>
           ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+        </div>
+
       </div>
     </nav>
   )
