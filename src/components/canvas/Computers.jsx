8120853d44dd8eb18ed3f545db9b03dd1a2129ae
Montywilliams <3650@holbertonschool.com>
2023-03-07 15:16:31 -0600

    import 3d model

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -3,6 +3,7 @@ import { canvas } from '@react-three/fiber';
 import { orbitControls, Preload, useGLTF } from '@react-three/drei';
 
 const Computers = () => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
     <div>Computers</div>
   )
