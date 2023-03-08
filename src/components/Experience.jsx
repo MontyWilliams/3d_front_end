154ddc309f67b2de8c5c7d7802c1c318d2a54d53
Montywilliams <3650@holbertonschool.com>
2023-03-08 13:52:56 -0600

    import requirements: will be using reacts timeline module in this comp

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,11 @@
-import React from 'react'
+import { VerticlTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { motion }  from 'framer-motion'
+import 'react-vertical-timeline-component/style.min.css'
+import { styles } from '../style.js'
+import { experiences } from '../constants'
+import { SectionWrapper } from '../hoc'
+import { textVariant } from '../utils/motion'
+
 
 const Experience = () => {
   return (
