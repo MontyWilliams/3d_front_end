15921282b5ca5efe081f156016a5844ab1abf571
Montywilliams <3650@holbertonschool.com>
2023-03-08 14:07:54 -0600

    correct typo: create experience card for maped data to be rendered in

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { VerticlTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
 import { motion }  from 'framer-motion'
 import 'react-vertical-timeline-component/style.min.css'
 import { styles } from '../style.js'
@@ -6,7 +6,13 @@ import { experiences } from '../constants'
 import { SectionWrapper } from '../hoc'
 import { textVariant } from '../utils/motion'
 
+const ExperienceCard = ({ experience}) => {
+  <VerticalTimelineElement
+    contentStyle={{ bckground: '#1d1836', color: '#fff' }}
+  >
 
+  </VerticalTimelineElement>
+}
 
 const Experience = () => {
   return (
@@ -15,6 +21,13 @@ const Experience = () => {
       <p className={styles.sectionSubText}>Check out what Ive done so far</p>
         <h2 className={styles.sectionHeadText}>work experience</h2>
       </motion.div>
+      <div className="mt-20 flex flex-col">
+        <VerticalTimeline>
+          {experiences.map((experience, index) => (
+            <ExperienceCard key={index} experience={experience} />
+          ))}
+        </VerticalTimeline>
+      </div>
     </>
   )
 }
