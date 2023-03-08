704a0950c4f4ea4c4e20cef8b63468159e6d473c
Montywilliams <3650@holbertonschool.com>
2023-03-08 12:54:02 -0600

    create section wrapper and import requirements

---
+++ hoc/SectionWrapper.jsx
@@ -0,0 +1,11 @@
+import { motion } from 'framer-motion'
+import { styles } from './style.js'
+import { staggeContainer } from '../utils/motion'
+
+const SectionWrapper = (Component, idName) => {
+    return (
+        <div>
+
+        </div>
+    )
+}
