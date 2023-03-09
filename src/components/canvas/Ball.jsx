c4c14f2be61d628df45ea596f30545131111b555
Montywilliams <3650@holbertonschool.com>
2023-03-09 15:30:07 -0600

    create canvas and render ball inside it: ball component wraps in float with ligh settings

--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -3,10 +3,33 @@ import { Canvas } from '@react-three/fiber'
 import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei'
 import CanvasLoader from '../Loader'
 
-const Ball = () => {
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl])
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1}>
+      <ambientLight intensity={0.25} />
+      <directionalLight position={[0, 0, 0.5]} />
+
+    </Float>
   )
 }
 
-export default Ball
+const BallCanvas = ({ icon }) => {
+  return (
+    <Canvas
+      frameloop='demand'
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          enableZoom={false}
+        />
+        <Ball imgUrl={icon} />
+      </Suspense>
+
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default BallCanvas
