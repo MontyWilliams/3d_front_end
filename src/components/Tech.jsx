fe08c4070041f0290118e0696023e146c09d9583
Montywilliams <3650@holbertonschool.com>
2023-03-09 15:13:35 -0600

    map through technologies and render a canvas for each instance

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -4,7 +4,13 @@ import { technologies } from '../constants'
 
 const Tech = () => {
   return (
-    <div></div>
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((tech) => (
+        <div className="w-28 h-28" key={tech.name}>
+          <BallCanvas icon={tech.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
