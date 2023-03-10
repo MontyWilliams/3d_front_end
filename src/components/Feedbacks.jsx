1671e070d13f15ff261fe57505b5900da25a217a
Montywilliams <3650@holbertonschool.com>
2023-03-10 03:23:24 -0600

    add styling and motion variant for testimonials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -6,7 +6,10 @@ import { fadeIn, textVariant } from '../utils/motion'
 import { testimonials } from '../constants'
 
 const FeedBackCard = ({ index, testimonial, name, designation, company, image}) => (
-  <motion.div>
+  <motion.div
+    variants={fadeIn("", "spring", index * 0.5, 0.75)}
+    className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full"
+  >
     <p>{testimonial}</p>
   </motion.div>
 )
