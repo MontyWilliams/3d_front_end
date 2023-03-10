b82c823ecdc516246a7b0369e0320ce4c07733cd
Montywilliams <3650@holbertonschool.com>
2023-03-10 01:11:32 -0600

    create motion paragraph for title and description: div should be wrapped in SectionWrapper

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -10,6 +10,23 @@ import { fadeIn, textVariant } from '../utils/motion'
 const Works = () => {
   return (
     <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>My Work</p>
+        <h2 className={styles.sectionHeadText}>Projects.</h2>
+      </motion.div>
+      <div className="w-full flex">
+        <motion.p
+          variants={fadeIn("", "", 0.1, 1)}
+          className="mt-3 text-secondary text-[17px] max-w-3xl leading-[30px]"
+        >
+          The following are not all the projects I have created but they showcase my skill
+          and understanding of the front and back end of web development. Each project includes 
+          a link to the github wher you will find a rich commit history detailing my thought process
+          through each step of the project as well as a link to a live demo of the project hosed 
+          online. It reflects a desire and ability to build and create applications using a variety of
+          tools.
+        </motion.p>
+      </div>
     </>
   )
 }
