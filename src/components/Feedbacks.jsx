b7a7faa11e56f215719f27a399832d9b8a0bf9cf
Montywilliams <3650@holbertonschool.com>
2023-03-10 03:18:33 -0600

    import requirements

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import { motion } from 'framer-motion'
+
+import { styles } from '../style'
+import { SectionWrapper } from '../hoc'
+import { fadeIn, textVariant } from '../utils/motion'
+import { testimonials } from '../constants'
 
 const Feedbacks = () => {
   return (
-    <div>Feedbacks</div>
+    <div className="mt-12 bg-black-100 rounded-[20px]"></div>   
   )
 }
 
-export default Feedbacks
+export default SectionWrapper(Feedbacks, "")
