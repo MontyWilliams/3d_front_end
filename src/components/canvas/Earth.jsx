0d4498748238ff4d4ef89641bddcd70ca68a1d5d
Montywilliams <3650@holbertonschool.com>
2023-03-10 14:46:34 -0600

    creat canvas for earht model: add orbitcontrols and lighting as well as falback for loading using new react 18 feature (suspense)

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -10,4 +10,25 @@ const Earth = () => {
   )
 }
 
-export default Earth
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop='demand'
+      gl={{ preserverDrawingBuffer: true }}
+      camera={{}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas
